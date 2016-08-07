f19024332ece52fb3415845493f3a51ef5391ee5
sumit <sgupta40@sapient.com>
2016-08-07 23:40:58 +0530

    Starter store for biz config keys

---
+++ src/main/java/com/mns/BizkeyApplication.java
@@ -0,0 +1,12 @@
+package com.mns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizkeyApplication.class, args);
+	}
+}
